87dc3dbca59dfa6b9f5def667bf00c58c4cd74c1
he530xlq <128357713+he530xlq@users.noreply.github.com>
2025-04-29 10:01:39 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -23,7 +23,7 @@ let go2Socks5s = [
 	'*.loadshare.org',
 ];
 let addresses = [];
-let addressesapi = [];
+let addressesapi = [https://raw.githubusercontent.com/he530xlq/yxcfip/refs/heads/main/ip.txt];
 let addressesnotls = [];
 let addressesnotlsapi = [];
 let addressescsv = [];
